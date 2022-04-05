fb1b242be10fb48d446671516b003a3851cf19a8
Lauma Sare <lauma.sare@gmail.com>
2022-04-05 15:08:13 +0300

    Third TA day with Individual Task2.

--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -110,7 +110,7 @@ public class Task2 {
 
         driver.findElement(By.cssSelector("input[value='" + gender + "']")).click();
 
-        WebElement dropdown = driver.findElement(By.id("vfb-12"));
+        WebElement dropdown = driver.findElement(By.id("like_us"));
         Select dropSelect = new Select(dropdown);
         dropSelect.selectByVisibleText(option);
 
@@ -120,6 +120,13 @@ public class Task2 {
 //         check fields are filled correctly
 
 
+        assertEquals(name, driver.findElement(By.id("name")).getText());
+        assertEquals(age, driver.findElement(By.id("age")).getText());
+        assertEquals(language, driver.findElement(By.id("language")).getText());
+        assertEquals(comment, driver.findElement(By.id("comment")).getText());
+
+        assertEquals(gender, driver.findElement(By.id("gender")).getText());
+        assertEquals(option, driver.findElement(By.id("option")).getText());
 
 
 //         check button colors
@@ -141,27 +148,89 @@ public class Task2 {
     public void yesOnWithNameFeedbackPage() throws Exception {
 //         TODO:
 //         enter only name
+        String name = "Liene";
+        driver.findElement(By.id("fb_name")).sendKeys(name);
+
 //         click "Send"
+        driver.findElement(By.tagName("button")).click();
+
 //         click "Yes"
+        driver.findElement(By.className("w3-green")).click();
+
 //         check message text: "Thank you, NAME, for your feedback!"
+
+        assertEquals("Thank you, " + name + ", for your feedback!", driver.findElement(By.id("message")).getText());
+
+
 //         color of text is white with green on the background
+        assertEquals("rgba(255, 255, 255, 1)", driver.findElement(By.id("message")).getCssValue("color"));
+        assertEquals("rgba(76, 175, 80, 1)", driver.findElement(By.className("w3-green")).getCssValue("background-color"));
     }
 
     @Test
     public void yesOnWithoutNameFeedbackPage() throws Exception {
 //         TODO:
 //         click "Send" (without entering anything
+        driver.findElement(By.tagName("button")).click();
 //         click "Yes"
+        driver.findElement(By.className("w3-green")).click();
+
 //         check message text: "Thank you for your feedback!"
+        String expectedMessage = "Thank you for your feedback!";
+        assertEquals(expectedMessage, driver.findElement(By.id("message")).getText());
+
 //         color of text is white with green on the background
+        assertEquals("rgba(255, 255, 255, 1)", driver.findElement(By.id("message")).getCssValue("color"));
+        assertEquals("rgba(76, 175, 80, 1)", driver.findElement(By.className("w3-green")).getCssValue("background-color"));
+
     }
 
     @Test
     public void noOnFeedbackPage() throws Exception {
-//         TODO:
+
+        String name = "Liene";
+        String age = "42";
+        String option = "Good";
+        String comment = "My comment";
+        String language = "English";
+        String gender = "female";
+
 //         fill the whole form
+        driver.findElement(By.id("fb_name")).sendKeys(name);
+        driver.findElement(By.id("fb_age")).sendKeys(age);
+        driver.findElement(By.tagName("textarea")).sendKeys(comment);
+
+        driver.findElement(By.cssSelector("input[value='" + language + "']")).click();
+
+        driver.findElement(By.cssSelector("input[value='" + gender + "']")).click();
+
+        WebElement dropdown = driver.findElement(By.id("like_us"));
+        Select dropSelect = new Select(dropdown);
+        dropSelect.selectByVisibleText(option);
+
 //         click "Send"
+        driver.findElement(By.tagName("button")).click();
+
 //         click "No"
+        driver.findElement(By.className("w3-red")).click();
+
 //         check fields are filled correctly (values are not clean)
+
+        assertEquals(name, driver.findElement(By.id("fb_name")).getAttribute("value"));
+        assertEquals(age, driver.findElement(By.id("fb_age")).getAttribute("value"));
+        assertEquals(comment, driver.findElement(By.tagName("textarea")).getAttribute("value"));
+
+//        assertEquals(language, driver.findElement(By.cssSelector("input[value='" + language + "']")).getAttribute("value"));
+        assertTrue(driver.findElement(By.cssSelector("input[value='" + language + "']")).isSelected());
+        assertTrue(driver.findElement(By.cssSelector("input[value='" + gender + "']")).isSelected());
+
+//
+        WebElement dropdownTwo = driver.findElement(By.id("like_us"));
+        Select backDrop = new Select(dropdownTwo);
+
+        List<WebElement> allSelect = backDrop.getAllSelectedOptions();
+        assertEquals(1, allSelect.size());
+        assertEquals(option, allSelect.get(0).getText());
+
     }
 }
